137d6bda417a0e50bc4cec28e3b75ba457b2628e
aaurdanet <antonioandres2000@gmail.com>
2025-02-22 19:30:46 -0500

    Initial commit

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class CdkProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+   
+      // Define the S3 bucket
+      const cdk_bucket = new Bucket(this, 'S3Bucket', {
+        bucketName: 'cdk-bucket-py123654',
+      });
+  
+      // Define your sources
+      const source_folders = [
+        s3deploy.Source.asset('./resources'), //remember to add one more . for windows
+      ];
+   
+      // Deploy a local folder to the S3 bucket
+      new s3deploy.BucketDeployment(this, 'DeployLocalFolder', {
+        sources: source_folders,
+        destinationBucket: cdk_bucket,
+      });
+    }
   }
-}
+
+ 
+
+ 
+ 
+
+ 
---
+++ resources/scripts/Caesar.py
@@ -0,0 +1,60 @@
+print(art.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction, key_count):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}, Key used: {key_count}")
+
+
+def brute_force_attack(start_text, cipher_direction):
+    shift = 0
+    for i in range(0, 26):
+        shift += 1
+
+        if shift > 26:
+            shift = shift % 26
+
+        caesar(start_text=start_text, shift_amount=shift, cipher_direction=cipher_direction, key_count=shift)
+
+
+continue_cipher = True
+
+while continue_cipher:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    if direction == "decode":
+        brute_force_selection = input("Would you like to brute force? Type 'Yes'. Otherwise Type 'No'\n").lower()
+    else:
+        brute_force_selection = "no"
+
+    text = input("Type your message:\n").lower()
+
+    if brute_force_selection == "yes":
+        brute_force_attack(start_text=text, cipher_direction=direction)
+
+    else:
+
+        shift = int(input("Type the shift number:\n"))
+        if shift > 26:
+            shift = shift % 26
+        caesar(start_text=text, shift_amount=shift, cipher_direction=direction, key_count=shift)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        continue_cipher = False
+        print("Goodbye")
+    elif restart == "yes":
+        continue_cipher = True
